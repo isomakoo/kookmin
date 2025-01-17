83edaa669109e101bb2cb57c84f30d04725eab42
isomakoo <161478022+isomakoo@users.noreply.github.com>
2025-01-17 17:08:46 +0500

    Yangi TUIT2025 broucherf loyihasi

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header-container">
+        <div className="header-list">
+          <ul className="header-navbar-list">
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Home
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                TUIT2025
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Program
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Registration
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Venu & Travel
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Commite
+              </a>
+            </li>
+            <li className="header-listes">
+              <a href="#" className="header-link">
+                Contact us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="section">
+          <div className="section-list">
+            <img src="https://www.icisct2024.org/images/1.png" alt="Tatu" /> <br />
+        
+          <a
+            href="https://www.icisct2024.org/icisct2024.png"
+            className="section-link"
+          >
+           <b> TUIT2025 Conference Brochure </b>
+          </a>
+          <br />
+          <img
+            src="https://www.icisct2024.org/images/TUIT_LOGO.png"
+            alt="TUIT logo"
+            className="logo-img"
+          />
+          <img
+            src="https://www.icisct2024.org/images/DT_LOGO.2.png"
+            alt="DTlogo"
+            className="logo-img"
+          />
+          <img src="https://www.icisct2024.org/images/KOOKMIN%20uni.gif" alt="kookmin"  className="images-kokmin"/>
+          </div>
+          <div className="section-listes">
+            <p className="section-title">
+              International Conference on Information Science and Communications
+              Technologies - Applications, Trends and Opportunities
+            </p> <br />
+            <b className="section-listes-text">
+              <a href="#">
+                {" "}
+                http://icisct2024.org/openconf/openconf.php
+              </a>{" "}
+              Paper submission is open Authors can attend or present paper(s)
+              online.
+            </b> <br />
+            <b className="section-listes-text">
+              ICISCT 2024 conference is co-organized by Kookmin University of
+              Republic of Korea, and is Sponsored technically by IEEE and IEEE
+              Photonics Society.
+            </b> <br />
+            <b className="section-listes-text">
+              Accepted papers will be submitted for inclusion in Engineering
+              Index (EI) and EI Compendex and Scopus and IEEE XploreTM. ISBN
+              numbers was assigned to accepted papers
+            </b> <br />
+            <p className="section-navbar-title">
+            XPLORE COMPLIANT:
+Part Number: CFP24H74-ART
+ISBN: 979-8-3315-2792-1
+            </p>
+            <b className="section-listes-textes">
+              7th, and 8th of November 2024, Kookmin University, Republic of
+              Korea at Kookmin University, South Korea Conference Venue @
+              Kookmin University, Republic of Korea
+            </b> <br />
+            <b className="section-listes-text">
+              Accepted papers will be published in the conference's proceedings
+              and/or USB: Part Number: CFP24H74-USB , ISBN: 979-8-3315-2791-4.
+            </b> <br />
+            <img src="https://www.icisct2024.org/images/ieeeps.png" alt="ieee" /> <br />
+            <img src="https://www.icisct2024.org/images/image001.gif" alt="" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
